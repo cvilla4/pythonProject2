cd142c84a307428efc87e090728f820db914423a
cvilla4 <neymarvilla1@gmail.com>
2023-10-10 14:02:44 -0500

    Escape Character

---
+++ main.py
@@ -0,0 +1,13 @@
+splitString = "This string has been \nsplit over\nseveral\nlines"
+# Fixed the quotation mark
+print(splitString)
+tabbedString = "1\t2\t3\t4\t5"
+# Fixed the quotation mark
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
+anotherString="""This string has been
+split over \
+several \
+lines""" # Fixed the quotation mark
+print(anotherString)
